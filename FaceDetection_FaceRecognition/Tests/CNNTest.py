1be24bd25ef97ec0fd898b88969fd3ef2a3fdfe1
DESKTOP-1AB3ELN\FinalFred <fred_bn2@yahoo.de>
2017-10-01 19:43:46 +0200

    Minor changes in CNN for theano backend, new pretrained weights (from https://github.com/fchollet/deep-learning-models/releases/tag/v0.1, vgg16_weights_th_dim_ordering_th_kernels.h5)

--- FaceDetection_FaceRecognition/Tests/CNNTest.py
+++ FaceDetection_FaceRecognition/Tests/CNNTest.py
@@ -2,49 +2,50 @@ from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
+from keras import backend
 import cv2
 import numpy as np
-import keras
+
 
 def VGG_16(weights_path=None):
-    #model = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
-    #return model
+    # model = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+    # return model
     model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, 224, 224)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2,2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(Flatten())
     model.add(Dense(4096, activation='relu'))
@@ -52,6 +53,7 @@ def VGG_16(weights_path=None):
     model.add(Dense(4096, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1000, activation='softmax'))
+    model.pop()
 
     if weights_path:
         model.load_weights(weights_path)
@@ -59,16 +61,20 @@ def VGG_16(weights_path=None):
     return model
 
 if __name__ == "__main__":
+    # 'tf' = tensorflow, 'th = theano
+    backend.set_image_dim_ordering('th')
     im = cv2.resize(cv2.imread('../data/test/selfie.jfif'), (224, 224)).astype(np.float32)
-    im[:,:,0] -= 103.939
-    im[:,:,1] -= 116.779
-    im[:,:,2] -= 123.68
-    im = im.transpose((2,0,1))
+    im[:, :, 0] -= 103.939
+    im[:, :, 1] -= 116.779
+    im[:, :, 2] -= 123.68
+    im = im.transpose((2, 0, 1))
     im = np.expand_dims(im, axis=0)
 
     # Test pretrained model
-    model = VGG_16('C:/VGG/vgg16_weights.h5')
+    # model = VGG_16('C:/VGG/vgg16_weights_th_dim_ordering_th_kernels.h5')
+    model = VGG_16()
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
     out = model.predict(im)
-    print(np.argmax(out))
+    print(np.argmax(out), '\n')
+    print(out.shape)
